6bc4bcba387c4a48937bc6d91c00e45f7487f772
Philip Oesterle-Pekrun <philipoesterlepekrun@gmail.com>
2026-08-14 10:52:50 +0200

    Fix compiler launcher logic and workflow
    Signed-off-by: Philip Oesterle-Pekrun <philipoesterlepekrun@gmail.com>

--- cmake/functions/four_c_set_clangcuda_mode.cmake
+++ cmake/functions/four_c_set_clangcuda_mode.cmake
@@ -5,16 +5,11 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-# This function unsets some properties to prevent the kokkos launch compiler from being used on a ${target}, and sets the clangcuda mode for the clangcuda++ compiler wrapper.
+# Kokkos uses the RULE_LAUNCH properties to redirect compilation through kokkos_launch_compiler.
+# Clear only those properties here. CMake compiler launchers such as ccache are independent and must
+# remain enabled on the target.
 # clangcuda_mode can be either CLANGCUDA_MODE_HOST or CLANGCUDA_MODE_DEVICE
 function(set_clangcuda_mode target clangcuda_mode)
-  set_target_properties(
-    ${target}
-    PROPERTIES CXX_COMPILER_LAUNCHER ""
-               C_COMPILER_LAUNCHER ""
-               CUDA_COMPILER_LAUNCHER ""
-               RULE_LAUNCH_COMPILE ""
-               RULE_LAUNCH_LINK ""
-    )
+  set_target_properties(${target} PROPERTIES RULE_LAUNCH_COMPILE "" RULE_LAUNCH_LINK "")
   target_compile_definitions(${target} PRIVATE ${clangcuda_mode})
 endfunction()
